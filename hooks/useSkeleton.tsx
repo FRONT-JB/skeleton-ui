7ff553a70f1cd97b3779b00809c73b8fe933c84c
Genie <jeonbg@kakao.com>
2022-07-24 00:31:09 +0900

    Change : useSkeleton

--- hooks/useSkeleton.tsx
+++ hooks/useSkeleton.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from 'react';
 
 const useSkeleton = (SkeletonComponent: () => ReactElement, limit: number) => {
+  if (limit <= 1) {
+    return <SkeletonComponent />;
+  }
   return new Array(limit)
     .fill(1)
     .map((_, index) => <SkeletonComponent key={index} />);
